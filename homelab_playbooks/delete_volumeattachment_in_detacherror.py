5946bb51b7c811a52d5b82c6c1b31c7965eff4a1
Tom Nowak <ionfury@gmail.com>
2024-01-25 23:33:46 -0500

    Print exception info

--- homelab_playbooks/delete_volumeattachment_in_detacherror.py
+++ homelab_playbooks/delete_volumeattachment_in_detacherror.py
@@ -11,9 +11,9 @@ def delete_volume_attachment(volume_attachment_name):
     """
     try:
         api_response = client.StorageV1Api().delete_volume_attachment(volume_attachment_name)
-        logging.info(f"VolumeAttachment {volume_attachment_name} deleted: {api_response}")
+        logging.info(f"delete_volumeattachment_in_detacherror: VolumeAttachment {volume_attachment_name} deleted: {api_response}")
     except Exception as e:
-        ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"Exception when calling StorageV1Api->delete_volume_attachment: {e}")
+        ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_volumeattachment_in_detacherror: Exception when calling StorageV1Api->delete_volume_attachment: {e}")
 
 def patch_volume_attachment_finalizers(volume_attachment_name):
     """
@@ -22,30 +22,30 @@ def patch_volume_attachment_finalizers(volume_attachment_name):
     body = {"metadata": {"finalizers": []}}  # Removing finalizers
     try:
         api_response = client.StorageV1Api().patch_volume_attachment(volume_attachment_name, body)
-        logging.info(f"VolumeAttachment {volume_attachment_name} patched: {api_response}")
+        logging.info(f"delete_volumeattachment_in_detacherror: VolumeAttachment {volume_attachment_name} patched: {api_response}")
     except Exception as e:
-        ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"Exception when calling StorageV1Api->patch_volume_attachment: {e}")
+        ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_volumeattachment_in_detacherror: Exception when calling StorageV1Api->patch_volume_attachment: {e}")
 
 @action
 def delete_volumeattachment_in_detacherror(event: PrometheusKubernetesAlert):
     """
     Deletes a volumeattachment only when it is in a detach error state.
     """
-    logging.info(f"Entered delete_volumeattachment_in_detacherror!")
+    logging.info(f"delete_volumeattachment_in_detacherror: Entered delete_volumeattachment_in_detacherror!")
 
     volume_attachment_name = event.alert.labels.get('volumeattachment')
 
     if volume_attachment_name is None:
-        raise ActionException(ErrorCodes.RESOURCE_NOT_FOUND, f"VolumeAttachment name not found in alert labels")
+        raise ActionException(ErrorCodes.RESOURCE_NOT_FOUND, f"delete_volumeattachment_in_detacherror: VolumeAttachment name not found in alert labels")
 
     try:
         volume_attachment_status: V1VolumeAttachmentStatus = client.StorageV1Api().read_volume_attachment_status(volume_attachment_name)
 
         if volume_attachment_status.detach_error is not None:
-            logging.info(f"detachError found in {volume_attachment_name}.  Deleting...")
+            logging.info(f"delete_volumeattachment_in_detacherror: detachError found in {volume_attachment_name}.  Deleting...")
             patch_volume_attachment_finalizers(volume_attachment_name)
         else:
-            logging.info(f"No detachError found in {volume_attachment_name}.")
+            logging.info(f"delete_volumeattachment_in_detacherror: No detachError found in {volume_attachment_name}.")
 
     except Exception as e:
-        raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"Failed to detect volume attachment status for {volume_attachment_name}")
+        raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_volumeattachment_in_detacherror: Failed to detect volume attachment status for {volume_attachment_name}: {e}")
