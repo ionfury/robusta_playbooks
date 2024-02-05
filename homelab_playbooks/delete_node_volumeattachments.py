c7a22929c294fb316d29270a5432e5e107afd61c
Tom <ionfury@gmail.com>
2024-02-04 21:45:08 -0500

    Update delete_node_volumeattachments.py

--- homelab_playbooks/delete_node_volumeattachments.py
+++ homelab_playbooks/delete_node_volumeattachments.py
@@ -18,7 +18,7 @@ def delete_node_volumeattachments(event: NodeEvent):
         if volume_attachment.spec.node_name == node.metadata.name:
             try:
                 logging.info(f"delete_node_volumeattachments: deleting & removing finalizers from volume_attachment {volume_attachment.metadata.name} from node {node.metadata.name}")
-                client.StorageV1Api().delete_volume_attachment(volume_attachment.metadata.name)
                 client.StorageV1Api().patch_volume_attachment(volume_attachment.metadata.name, body)
+                client.StorageV1Api().delete_volume_attachment(volume_attachment.metadata.name)
             except Exception as e:
-                raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_node_volumeattachments: failed to delete node volumeattachment {volume_attachment.metadata.name}")
+                raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_node_volumeattachments: failed to delete node volumeattachment {volume_attachment.metadata.name}: {e}")
