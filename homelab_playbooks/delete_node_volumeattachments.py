0df64a8f82b6d40a12b7f8d6d18827c61a452865
Tom Nowak <ionfury@gmail.com>
2024-02-04 19:44:56 -0500

    Add playbook: delete_node_volumeattachments

---
+++ homelab_playbooks/delete_node_volumeattachments.py
@@ -0,0 +1,27 @@
+import logging
+
+from typing import List
+from robusta.api import NodeEvent, ErrorCodes, ActionException, action
+
+from kubernetes import client
+
+@action
+def delete_node_volumeattachments(event: NodeEvent):
+    """
+    When a node is deleted, delete all related volume attachments, removing any finalizers.
+    """
+    body = {"metadata": {"finalizers": None}}  # Removing finalizers
+    node = event.get_node()
+
+    if node:
+        raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_node_volume_attachments: node still exists.")
+
+    volume_attachments = client.StorageV1Api().list_volume_attachment()
+    for volume_attachment in volume_attachments.items:
+        if volume_attachment.spec.node_name == node.name:
+            try:
+                logging.info(f"delete_node_volumeattachments: deleting & removing finalizers from volume_attachment {volume_attachment.metadata.name} from node {node.name}")
+                client.StorageV1Api().delete_volume_attachment(volume_attachment.metadata.name)
+                client.StorageV1Api().patch_volume_attachment(volume_attachment.metadata.name, body)
+            except Exception as e:
+                raise ActionException(ErrorCodes.ACTION_UNEXPECTED_ERROR, f"delete_node_volumeattachments: failed to delete node volumeattachment {volume_attachment.metadata.name}")
